489fd2d2913fdd07e30c97d1572f4c06af3a1bb5
Roottoot <syuleiman@gmail.com>
2025-09-17 11:37:23 +0200

    Tests changed 4

--- backend/src/test/java/org/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/example/backend/BackendApplicationTests.java
@@ -3,11 +3,13 @@ package org.example.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-        "app.url=http://localhost",
-        "spring.security.oauth2.client.registration.github.client-id=test-id",
-        "spring.security.oauth2.client.registration.github.client-secret=test-secret"
-})
+@SpringBootTest(
+//        properties = {
+//        "app.url=http://localhost",
+//        "spring.security.oauth2.client.registration.github.client-id=test-id",
+//        "spring.security.oauth2.client.registration.github.client-secret=test-secret"
+//}
+)
 class BackendApplicationTests {
     @Test
     void contextLoads() {
